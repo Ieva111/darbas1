866ba6cd2bec34a511050b52025445dfe18379c8
Ieva111 <61240009+Ieva111@users.noreply.github.com>
2021-05-30 12:31:11 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -19,6 +19,14 @@ def duk():
 def rez():
     return render_template('rezultatas.html')
 
+@app.route('/rezultatas1')
+def rez1():
+    return render_template('rezultatas1.html')
+
+@app.route('/rezultatas2')
+def rez2():
+    return render_template('rezultatas2.html')
+
 @app.route('/irankis')
 def irankis():
     return render_template('irankis.html')
